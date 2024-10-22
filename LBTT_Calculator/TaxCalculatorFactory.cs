3d8fc63a8a395ab1c6d8f5263071652b807df6f2
Marcin Mucha <marcin.mucha@bjss.com>
2024-10-22 17:17:16 +0100

    added TaxCalcualtorFactory

---
+++ LBTT_Calculator/TaxCalculatorFactory.cs
@@ -0,0 +1,16 @@
+﻿using LBTT_Calculator.Output;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBTT_Calculator
+{
+    public class TaxCalculatorFactory
+    {
+        public StandardCalculator CreateStandardResidential() { return new StandardCalculator(); }
+        public FirstTimeBuyerCalculator CreateFirstTimeBuyer() { return new FirstTimeBuyerCalculator(); }
+
+    }
+}
