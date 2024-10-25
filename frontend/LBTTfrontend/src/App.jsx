4d0f64109db9977bb2a9dbc8393cc7e191535231
Marcin Mucha <marcin.mucha@bjss.com>
2024-10-25 17:27:12 +0100

    fixed burger menu

--- frontend/LBTTfrontend/src/App.jsx
+++ frontend/LBTTfrontend/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TransactionForm from './TransactionForm'
+import NavBar from './NavBar';
 
 const App = () => {
   return (
-    <div id="app">
-      <TransactionForm />
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<div className="container mt-5 text-center"><h1>Welcome to LBTTax</h1><p>Your one-stop solution for tax calculations.</p></div>} />
+        <Route path="/calculator" element={<TransactionForm />} />
+      </Routes>
+    </Router>
   );
 };
 
